6a6bb27953ce2b5f45f7459d323ec380f07b5fb4
domingo79 <andria140779@yahoo.it>
2021-07-30 15:14:19 +0200

    test api in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('posts', function () {
+    $posts = Post::with(['tags'])->paginate();
+    return $posts;
 });
